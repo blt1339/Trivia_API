3564d33197f9db7488e5dd6b0116ec2e15c139b9
Beverly Thompson <blt1339@gmail.com>
2021-07-19 18:35:43 -0400

    test_flaskr.py

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -40,7 +40,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['total_questions'])
-        self.assertTrue(len(data['books']))
+        self.assertTrue(len(data['questions']))
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
